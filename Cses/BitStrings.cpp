7561e0a2be10df9d09564075039f9168df4d4339
Hissane <mouna.hissane@um5s.net.ma>
2019-10-25 14:25:24 +0100

    Update .gitignore

--- Cses/BitStrings.cpp
+++ Cses/BitStrings.cpp
@@ -3,17 +3,10 @@
 using namespace std;
 
 int main(){
-	long n, res = 1; 
-	long m = 10e9 + 7, x;
-	cin >> n; 
-	x = 2;
-	while(n > 0){
-		if(n % 2 != 0){
-			res = (res * x) % m;
-		}
-		n = n >> 1; 
-		x = (x*2) % m;
+	long n, res = 1;
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		res = (res*2)%1000000007;
 	}
 	cout << res;
-	return 0;
 }
