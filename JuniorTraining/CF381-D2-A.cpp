d172ce1e893d72845192dbd7b230ee176187284a
Hissane <mouna.hissane@um5s.net.ma>
2020-01-10 23:34:08 +0100

    add sereja and dima

--- JuniorTraining/CF381-D2-A.cpp
+++ JuniorTraining/CF381-D2-A.cpp
@@ -4,30 +4,32 @@ using namespace std;
 
 int main(){
 	int n, score1 = 0, score2 = 0, val, sum = 0; 
-	vector<int> v;
+	list<int> v;
 
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		cin >> val;
 		v.push_back(val);
 	}
-	for(auto x : v){
-		cout << x << " ";
-	}
-	cout << endl;
-	sort(v.begin(), v.end(), greater<int>());
-	for(auto x : v){
-		cout << x << " ";
-	}
-	cout << endl;
 	for(int i = 0; i < n; i++){
 		if( i % 2 == 0){
-			score1 = score1 + v[i];
+			if(v.front() > v.back()){
+				score1 = score1 + v.front();
+				v.pop_front();
+			}else{
+				score1 = score1 + v.back();
+				v.pop_back();
+			}
 		}else{
-			score2 = score2 + v[i];
+			if(v.front() > v.back()){
+				score2 = score2 + v.front();
+				v.pop_front();
+			}else{
+				score2 = score2 + v.back();
+				v.pop_back();
+			}
 		}
-		sum += v[i];
 	}
-	cout << score1 << " "<< score2 << " "<< sum;
+	cout << score1 << " "<< score2;
 	return 0;
 }
