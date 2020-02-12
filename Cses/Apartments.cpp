88f3116b7d78290d8e019ea4502ee7ea45f55d19
Hissane <mouna.hissane@um5s.net.ma>
2020-02-12 02:19:14 +0100

    not all test cases are correct

--- Cses/Apartments.cpp
+++ Cses/Apartments.cpp
@@ -3,9 +3,9 @@
 using namespace std; 
 
 int main(){
-	int n, k, m, res = 0, j, stop = 0, val;
-	vector<int> v1;
-	vector<int> v2;
+	long long n, k, m, res = 0, j, stop = 0,  val;
+	vector<long long> v1;
+	vector<long long> v2;
 	cin >> n >> m >> k;
 	for(int i = 0; i < n; i++){
 		cin >> val;
@@ -21,18 +21,22 @@ int main(){
 	
 	j = 0;
 	for(int i = 0; i < n; i++){
+		cout << v1[i] << " " << v2[j] <<endl;
+		if(v1[i] > k + v2[j]){
+			j++;
+		}
+		if(v2[j] > k + v1[i]){
+			i++;
+		}
 		if(abs(v1[i] - v2[j]) <= k){
 			res++;
 			j++;
 			stop = i;
 		}
-		
 		if(i == n - 1){
 			i = stop + 1;
-			j++;
-			
+			j++;	
 		}
-		
 	}
 	cout << res;
 	return 0;
