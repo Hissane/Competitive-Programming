aea103d441b91c95e5366d47a7ca1780d0eea719
Hissane <mouna.hissane@um5s.net.ma>
2020-02-17 19:00:12 +0100

    hashing with a map

--- HackerEarth/hashing.cpp
+++ HackerEarth/hashing.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main(){
 	int n, q, val;
 	string s;
-	string arr[1000000005];
+	map<int, string> map;
 
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		cin >> val >> s;
-		arr[val] = s;
+		map[val] = s;
 	}
 	cin >> q;
 	for(int i = 0; i < q; i++){
 		cin >> val;
-		cout << arr[val] << endl;
+		cout << map[val] << endl;
 	}
 	return 0;
 }
