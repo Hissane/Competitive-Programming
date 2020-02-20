72e373fb11bf7c915a1c865f94548d9e172c8fd6
Hissane <mouna.hissane@um5s.net.ma>
2020-02-20 16:01:58 +0100

    final version

--- JuniorTraining/CF567-D2-A.cpp
+++ JuniorTraining/CF567-D2-A.cpp
@@ -4,23 +4,20 @@ using namespace std;
 
 int main(){
 	int n, val;
-	int arr[10005];
+	int arr[100005];
 	int max[100005];
 	int min[100005];
 	cin >> n;
 	for(int i = 1; i <= n; i++){
 		cin >> arr[i];
 	}
-	for(int i = 1; i <= n/2 ; i++){
-		max[i] = arr[n] - arr[i];
-	}
-	for(int i = n/2 + 1 ; i <= n; i++){
-		max[i] = arr[i] - arr[1];
-	}
 	min[1] = arr[2] - arr[1];
 	min[n] = arr[n] - arr[n-1];
+	max[1] = arr[n] - arr[1];
+	max[n] = arr[n] - arr[1];
 	for(int i = 2; i < n; i++){
 		min[i] = ((arr[i] - arr[i-1]) > (arr[i+1] - arr[i]) ? arr[i+1] - arr[i] : arr[i] - arr[i-1]);
+		max[i] = ((arr[i] - arr[1]) > (arr[n] - arr[i]) ? arr[i] - arr[1] : arr[n] - arr[i]);
 	}
 	for(int i = 1; i <= n; i++){
 		cout << min [i] << " " << max[i] << endl;
