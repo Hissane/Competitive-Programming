300e2ef8c498ec0453b611e4495008d7ea21f8d3
Hissane <mouna.hissane@um5s.net.ma>
2020-04-22 23:48:49 +0000

    fixing some issues

---
+++ JuniorTraining/UVA 11231.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int m, n, c, res;
+	while (cin >> n >> m >> c && m != 0){
+		if(m == n){
+			if(c == 1){
+				res = ceil((n-7)/2.0)*ceil((m-7)/2.0);
+			}else{
+				res = ceil((n-8)/2.0)*ceil((m-8)/2.0);
+			}
+		}else{
+			c = (c + (max(m,n) - min(m,n))%2)%2;
+			if(c == 1){
+				res = ceil((n-7)/2.0)*ceil((m-7)/2.0);
+			}else{
+				res = ceil((n-8)/2.0)*ceil((m-8)/2.0);
+			}
+		}
+		cout << res << endl;
+	}
+	return 0;
+}
