f6d8e9f7aa5d2923bcdcfa6a81fdddaf7b27538b
Hissane <mouna.hissane@um5s.net.ma>
2020-04-24 20:58:12 +0000

    add black and white

--- JuniorTraining/UVA 11231.cpp
+++ JuniorTraining/UVA 11231.cpp
@@ -5,20 +5,7 @@ using namespace std;
 int main(){
 	int m, n, c, res;
 	while (cin >> n >> m >> c && m != 0){
-		if(m == n){
-			if(c == 1){
-				res = ceil((n-7)/2.0)*ceil((m-7)/2.0);
-			}else{
-				res = ceil((n-8)/2.0)*ceil((m-8)/2.0);
-			}
-		}else{
-			c = (c + (max(m,n) - min(m,n))%2)%2;
-			if(c == 1){
-				res = ceil((n-7)/2.0)*ceil((m-7)/2.0);
-			}else{
-				res = ceil((n-8)/2.0)+ceil((m-8)/2.0);
-			}
-		}
+		res = ((n-7)*(m-7) + c)/2;
 		cout << res << endl;
 	}
 	return 0;
