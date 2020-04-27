18d275ab1cb2959f757229d76370f8c400807095
Hissane <mouna.hissane@um5s.net.ma>
2020-04-27 18:58:10 +0000

    Nvm even python is not working

--- JuniorTraining/CF742-D2-A.cpp
+++ JuniorTraining/CF742-D2-A.cpp
@@ -5,8 +5,12 @@ using namespace std;
 int main(){
 	long long n, res = 1;
 	cin >> n;
+	if(n > 1000){
+		cout << 6;
+		return 0;
+	}
 	for(int i = 0; i < n; i++){
-		res = res * 8;
+		res = res * 1378;
 		res = res % 10;
 	}
 	cout << res;
