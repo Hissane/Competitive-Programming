aaaf5ae741c667b018ad467672c19f8fead2869a
Hissane <mouna.hissane@um5s.net.ma>
2020-04-28 22:39:33 +0000

    rubbish2

--- Rounds/CF636-D3-A.cpp
+++ Rounds/CF636-D3-A.cpp
@@ -7,14 +7,7 @@ int main(){
 	cin >> t;
 	for(int i = 0; i < n; i++){
 		cin >> n;
-		int j = 2;
-		while(x*(j - 1) < n){
-			if(x == n){
-				cout << n / (i -1);
-				break;
-			}
-			i = i*2;
-		}
+		
 	}
 	return 0;
 }
