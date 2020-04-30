a3bffef680063108ebc0fba008c8e1a3f6eecb1a
Hissane <mouna.hissane@um5s.net.ma>
2020-04-30 22:46:33 +0000

    fixing bugs

--- JuniorTraining/CF742-D2-A.cpp
+++ JuniorTraining/CF742-D2-A.cpp
@@ -5,14 +5,9 @@ using namespace std;
 int main(){
 	long long n, res = 1;
 	cin >> n;
-	if(n > 1000){
-		cout << 6;
-		return 0;
-	}
 	for(int i = 0; i < n; i++){
 		res = res * 1378;
-		res = res % 10;
+		cout << i + 1 << " " << res << endl;
 	}
-	cout << res;
 	return 0;
 }
