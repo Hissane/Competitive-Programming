6d29c5a1570f5bb30c63747befb937a09fb98603
Hissane <mouna.hissane@um5s.net.ma>
2020-05-05 22:57:40 +0000

    i guess its working now ?

--- Arena/fibo.cpp
+++ Arena/fibo.cpp
@@ -2,26 +2,24 @@
 
 using namespace std;
 
-map<int, int> map;
+map<int, int> my_map;
 
-int fibo(int n){
-	
-	if(map[n] == 0){
-		if(n == 1 || n == 0){
-			map[n] = 1;
+int fibo(int n){	
+	if(my_map[n] == 0){
+		if(n == 1 || n == 2){
+			my_map[n] = 1;
 		}else{
-			map[n] = fibo(n-1) + fibo(n-2);
+			my_map[n] = fibo(n-1) + fibo(n-2);
 		}
 	}
-	return map[n];
+	return my_map[n];
 }
 
 int main(){
-	int q, n;
-	
-	cin >> n;
+	int q, n;	
+	cin >> q;
 	for(int i = 0; i < q; i++){
-		cin >> q;
+		cin >> n;
 		cout << fibo(n) << endl;	
 	}
 	return 0;
