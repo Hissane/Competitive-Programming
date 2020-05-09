8925d6a38ab660633981e51ade7bad5465246840
Hissane <mouna.hissane@um5s.net.ma>
2020-05-09 09:24:31 +0000

    trying hackerrank for a change

---
+++ Hackerrank/2DArray-DS.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int res;
+    int count = 0, max = -70;  
+    int arr[7][7];
+
+    for(int i = 0; i < 6; i++){
+      for(int j = 0; j < 6; j++){
+        cin >> arr[i][j];
+      }
+    }
+    for(int i = 0; i < 4; i++){
+      for(int j = 0; j < 4; j++){
+          count = arr[i][j] + arr[i][j+1] + arr[i][j+2]+ arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+          if(count > max){
+              max = count;
+          }
+      }
+    } 
+    cout << max; 
+    return 0;
+}
