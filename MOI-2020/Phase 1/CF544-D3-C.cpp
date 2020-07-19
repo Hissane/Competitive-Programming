951323ce6def3bf7cce9aed7e47870f338674f75
Hissane <mouna.hissane@um5s.net.ma>
2020-07-19 20:03:53 +0100

    Two pointers

--- MOI-2020/Phase 1/CF544-D3-C.cpp
+++ MOI-2020/Phase 1/CF544-D3-C.cpp
@@ -3,31 +3,22 @@
 using namespace std; 
 
 int main(){
-	int n;
-	//int arr[200005];
+	int n, val;
 	vector<int> arr;
-	int res2 = 0, res = 1, val;
-
+	
 	cin >> n; 
 	for(int i = 0; i < n; i++){
 		cin >> val;
 		arr.push_back(val);
 	}
 	sort(arr.begin(), arr.end());
-	int p1 = 0, p2 = 1;
+	int j = 0, ans = 0;
 	for(int i = 0; i < n ; i++){
-		if((arr[p2] - arr[p1]) <= 5 && p2 < n){
-			p2++;
-			res++;
-		}else{
-			p1 = p2; 
-			p2 = p1+1;
-			if(res > res2){
-				res2 = res;
-			}
-			res = 1;
+		while(j < n && (arr[j] - arr[i]) <= 5){
+			j++;
+			ans = (j - i > ans ? j - i : ans);
 		}
 	}
-	cout << (res2 > res ? res2 : res);
+	cout << ans; 
 	return 0;
 }
