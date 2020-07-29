21dfcc186b1ec5bcb356a19e014d8debbbb5ed95
Hissane <mouna.hissane@um5s.net.ma>
2020-07-29 15:43:39 +0100

    change location

--- JuniorTraining/UVA 352.cpp
+++ JuniorTraining/UVA 352.cpp
@@ -3,26 +3,23 @@
 using namespace std;
 
 int visited[30][30] = {0};
-int arr[30][30] = {0};
 
-int counter(int i, int j, int n){
+int counter(int i, int j){
 	visited[i][j] = 1;
 	for(int k = -1; k < 2; k++){
 		for(int l = -1; l < 2; l++){
-			if(arr[i+k][j+l] = 1 && visited[i+k][j+l] == 0 && (i+k) > -1 && (i+k) < n && (j+l) > -1 && (j+l) < n){
-				counter(i+k, j+l, n);
+			if(arr[i+k][j+l] = 1 && visited[i+k][j+l] == 0){
+				counter(i+k, j+l);
 			}
 		}
 	}
-	return 0;
 }
 
 int main(){
-	int n;
+	int n, res = 0;
 	while(cin >> n){
-		int res = 0;
-		arr[30][30] = {0};
-		visited[30][30] = {0};
+		int arr[30][30] = {0};
+		visted[30][30] = {0};
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
 				cin >> arr[i][j];
@@ -31,7 +28,7 @@ int main(){
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
 				if(arr[i][j] = 1 && visited[i][j] == 0){
-					counter(i, j, n);
+					counter(i, j);
 					res++;
 				}
 			}
