2064a228c635bda857f2c1d86fb0326e307a0554
Hissane <mouna.hissane@um5s.net.ma>
2020-08-25 01:35:18 +0100

    :tada: simple flood fill problem

--- JuniorTraining/UVA 352.cpp
+++ JuniorTraining/UVA 352.cpp
@@ -2,7 +2,7 @@
 
 using namespace std; 
 
-int arr[30][30];
+char arr[30][30];
 int visited[30][30];
 int n;
 
@@ -10,7 +10,7 @@ void dfs(int i, int j){
 	visited[i][j] = 1;
 	for(int k = -1; k < 2; k++){
 		for(int l = -1; l < 2; l++){
-			if(i+k < n && i+k > 0 && i+k > 0 && j+l < n && visited[i+k][j+l] == 0 && arr[i+k][j+l] == 1){
+			if(i+k < n && i+k >= 0 && i+k >= 0 && j+l < n && visited[i+k][j+l] == 0 && arr[i+k][j+l] == '1'){
 				dfs(i+k,j+l);
 			}
 		}
@@ -29,7 +29,7 @@ int main(){
 		}
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
-				if(visited[i][j] == 0 && arr[i][j] == 1){
+				if(visited[i][j] == 0 && arr[i][j] == '1'){
 					dfs(i,j);
 					res++;
 				}
