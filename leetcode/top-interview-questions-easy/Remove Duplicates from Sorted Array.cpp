25fb6d5e98cf8affd4c4303a2eeeb7ab21c7b0b6
Hissane <mouna.hissane@um5s.net.ma>
2020-10-26 23:12:28 +0100

    Accepted

--- leetcode/top-interview-questions-easy/Remove Duplicates from Sorted Array.cpp
+++ leetcode/top-interview-questions-easy/Remove Duplicates from Sorted Array.cpp
@@ -1,26 +1,27 @@
 class Solution {
-public: //[1,1,2,2,2,3,3,3,3,4,5,5]
+public:
     int removeDuplicates(vector<int>& nums) {
         int len = nums.size();
-        int prev = nums[0];
         for(int i = 1; i < nums.size(); i++){
-            prev = nums[i-1];
+            int prev = nums[i-1];
             if(prev == nums[i]){
                 int k = 0;
                 while(((i+k) < nums.size()) && prev == nums[i+k]){
                     k++;
                     len--;
                 }
-                //len-=k;
-                nums.resize(nums.size()-k);
                 for(int j = i; (j+k) < nums.size(); j++){
                     nums [j] = nums [j+k];
                 }
+                nums.resize(nums.size()-k);
             }
         }
-        cout << len << ", nums = [" << nums[0];
-        for(int i = 1; i < len; i++){
-           cout << "," << nums[i];     
+        cout << len << ", nums = [";
+        for(int i = 0; i < len-1; i++){
+           cout << nums[i]<< ",";
+        }
+        if(len){
+            cout << nums[len-1];
         }
         cout << "]";
         return len; 
