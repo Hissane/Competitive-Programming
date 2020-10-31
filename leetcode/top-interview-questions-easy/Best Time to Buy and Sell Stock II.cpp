b649f27b509ce81b3941afabc182af68dbf21eee
Hissane <mouna.hissane@um5s.net.ma>
2020-10-31 18:54:55 +0100

    :white_check_mark: Cool problem, cool PoC

--- leetcode/top-interview-questions-easy/Best Time to Buy and Sell Stock II.cpp
+++ leetcode/top-interview-questions-easy/Best Time to Buy and Sell Stock II.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       
-    }
+        int sum = 0;
+        int peak = prices[0], val = prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            if(peak < prices[i]){
+                peak = prices[i];
+            }else{
+                sum += peak - val;
+                val = prices[i];
+                peak = prices[i];
+            }
+            if( i == prices.size() - 1){
+                sum += peak - val;
+            }
+        }
+        return sum;
+    }    
 };
