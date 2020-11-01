77e098af5893e9848dd206e8fc5e6d6ac12daa8d
Hissane <mouna.hissane@um5s.net.ma>
2020-11-01 23:20:46 +0100

    :construction: Not ready yet

---
+++ leetcode/top-interview-questions-easy/Move Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                count++;
+            }
+            nums.erase(nums.begin()+i);
+        }
+        for(int i = 0; i < count; i++){
+            nums.push_back(0);
+        }
+    }
+};
