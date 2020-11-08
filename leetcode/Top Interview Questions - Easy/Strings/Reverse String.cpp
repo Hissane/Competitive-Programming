43e45b78463de0f7f9b572d2e4c7b6affa032c2a
Hissane <mouna.hissane@um5s.net.ma>
2020-11-08 20:34:49 +0100

    :white_check_mark: Easy problem

--- leetcode/Top Interview Questions - Easy/Strings/Reverse String.cpp
+++ leetcode/Top Interview Questions - Easy/Strings/Reverse String.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
+        int n = s.size();
+        int temp;
+        for(int i = 0; i < n/2; i++){
+            temp = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = temp;
+        } 
     }
 };
