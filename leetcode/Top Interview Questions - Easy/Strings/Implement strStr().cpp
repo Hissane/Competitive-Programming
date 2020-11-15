8b515dc9f556e664f7dcc8237f1a28996bdcfe1a
Hissane <mouna.hissane@um5s.net.ma>
2020-11-15 22:06:25 +0100

    :construction_worker: Gotta implement KMP in order for it to work

---
+++ leetcode/Top Interview Questions - Easy/Strings/Implement strStr().cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle == ""){
+            return 0;
+        }
+        if(needle.length() > haystack.length()){
+            return -1;
+        }
+        for(int i = 0; i < haystack.length(); i++){
+            if(haystack[i] == needle[0]){
+                int l = i;
+                int j = 0;
+                while(haystack[l] == needle[j] && j < needle.length() && l < haystack.length()){
+                    l++;
+                    j++;
+                }
+                if(j == needle.length()&& l <= haystack.length()){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
