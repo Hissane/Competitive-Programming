1208d240ed634b0c2c1725e1374103bba2282645
Hissane <mouna.hissane@um5s.net.ma>
2020-11-21 23:21:16 +0100

    :white_check_mark: made a dummy implementation but still works mn d9a lwla

--- leetcode/Top Interview Questions - Easy/Array/Valid Sudoku.cpp
+++ leetcode/Top Interview Questions - Easy/Array/Valid Sudoku.cpp
@@ -22,15 +22,20 @@ public:
                 }
             }
         }
-        for(int i = 0; i < 9; i++){
-            map<char, int> map;
-            for(int j = 0; j < 9; j++){
-                if(board[i][i+j] != '.' && map[board[i][j]]){
-                    return false;
-                }else{
-                    map[board[i][j]]++;
+        for(int i = 0; i < 9; i = i + 3){
+            for(int j = 0; j < 9; j = j +3){
+              map<char, int> map;
+                for(int k = 0; k < 3; k++){
+                    for(int l = 0; l < 3; l++){
+                        if(board[i+k][j+l] != '.' && map[board[i+k][j+l]]){
+                            return false;
+                        }else{
+                            map[board[i+k][j+l]]++;
+                        }
+                    }
                 }
             }
         }
+        return true;
     }
 };
