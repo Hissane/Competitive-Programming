83c7ec023e7f4721b90ea99d222f88d2b75e27cd
Hissane <mouna.hissane@um5s.net.ma>
2020-12-05 08:31:13 +0100

    :white_check_mark: Pretty easy

--- leetcode/Top Interview Questions - Easy/Strings/Count and Say.cpp
+++ leetcode/Top Interview Questions - Easy/Strings/Count and Say.cpp
@@ -6,7 +6,7 @@ public:
             s = "1";
         }else{
             string str = countAndSay(n-1); 
-            for(int i = 0; i < str.length()-1; i++){
+            for(int i = 0; i < str.length(); i++){
                 int count = 1;
                 while(i < str.length()-1 && str[i] == str[i+1]){
                     count++;
