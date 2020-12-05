532879822d091c570eb406d417881c49a93713fb
Hissane <mouna.hissane@um5s.net.ma>
2020-12-05 08:26:25 +0100

    Still working on it

---
+++ leetcode/Top Interview Questions - Easy/Strings/Count and Say.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s = "";
+        if( n == 1 ){
+            s = "1";
+        }else{
+            string str = countAndSay(n-1); 
+            for(int i = 0; i < str.length()-1; i++){
+                int count = 1;
+                while(i < str.length()-1 && str[i] == str[i+1]){
+                    count++;
+                    i++;
+                }
+                s += to_string(count) + str[i];
+            }
+        }
+        return s;
+    }
+};
