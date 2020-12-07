5c170e59abfbefc7277e50179426cd00e5509dba
Hissane <mouna.hissane@um5s.net.ma>
2020-12-07 08:22:28 +0100

    :white_check_mark: implementation problem

---
+++ leetcode/Top Interview Questions - Easy/Math/Roman to Integer.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            switch(s[i]){
+                case 'I':
+                    if(i < s.length()-1 && s[i+1] == 'V'){
+                        sum += 4;
+                        i++;
+                    }else if(i < s.length()-1 && s[i+1] == 'X'){
+                        sum += 9;
+                        i++;
+                    }else{
+                        sum += 1;
+                    }
+                    break;
+                case 'V':
+                    sum += 5;
+                    break;
+                case 'X':
+                    if(i < s.length()-1 && s[i+1] == 'L'){
+                        sum += 40;
+                        i++;
+                    }else if(i < s.length()-1 && s[i+1] == 'C'){
+                        sum += 90;
+                        i++;
+                    }else{
+                        sum += 10;
+                    }
+                    break;
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'C':
+                    if(i < s.length()-1 && s[i+1] == 'D'){
+                        sum += 400;
+                        i++;
+                    }else if(i < s.length()-1 && s[i+1] == 'M'){
+                        sum += 900;
+                        i++;
+                    }else{
+                        sum += 100;
+                    }
+                    break;
+                case 'D':
+                    sum += 500;
+                    break;
+                case 'M':
+                    sum += 1000;
+                    break;
+            }
+        }
+        return sum;
+    }
+};
