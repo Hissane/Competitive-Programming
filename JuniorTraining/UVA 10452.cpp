940ccd13601905728c0348f3baf88f15294930cc
Hissane <mouna.hissane@um5s.net.ma>
2020-12-09 05:57:11 +0100

    gotta check it later

---
+++ JuniorTraining/UVA 10452.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main(){
+
+	int t, m, n, ms, ns; 
+	char arr[50][50];
+	char val;
+	string v = "IEHOVA#";
+	cin >> t;
+
+	for(int i = 0; i < t; i++){
+		
+		cin >> m >> n;
+		for(int j = 0; j < m; j++){
+			for(int k = 0; k < n; k++){
+				cin >> val;
+				arr[j][k] = val;
+				if(val == '@'){
+					ms = j;
+					ns = k;
+				}
+			}
+		}
+		int o = 0;
+		while(o < 7){
+			if(ns-1 >= 0 && arr[ms][ns-1] == v[o]){
+				cout << "left ";
+				ns = ns-1;
+			}else if(ns+1 < n && arr[ms][ns+1] == v[o]){
+				cout << "right ";
+				ns = ns+1;
+			}else if(ms-1 >= 0 && arr[ms-1][ns] == v[o]){
+				cout << "forth ";
+				ms = ms-1;
+			}else if(ms+1 < m && arr[ms+1][ns] == v[o]){
+				cout << "forth ";
+				ms = ms+1;
+			}
+			o++;
+		}
+		if(i < t-1){
+			cout << endl;
+		}
+	}
+
+
+	return 0;
+}
